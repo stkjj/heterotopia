cac2754c1492949ff4846ffbff8a1b80e370241f
stkjj <stefan@redaktiv.de>
2023-11-24 10:46:52 +0300

    1.5.8
    CF7 load optimization
    
    Signed-off-by: stkjj <stefan@redaktiv.de>

--- functions.php
+++ functions.php
@@ -73,3 +73,24 @@ function het_astra_single_post_meta( $dummy ) {
     return $output_str;
 }
 
+// Load CF stuff only where needed //
+
+function contactform_dequeue_scripts() {
+    $load_scripts = false;
+
+    if( is_singular() ) {
+        $post = get_post();
+
+        if( has_shortcode($post->post_content, 'contact-form-7') ) {
+            $load_scripts = true;
+            
+        }
+    }
+
+    if( ! $load_scripts ) {
+        wp_dequeue_script( 'contact-form-7' );
+        wp_dequeue_script('google-recaptcha');
+        wp_dequeue_style( 'contact-form-7' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'contactform_dequeue_scripts', 99 );
